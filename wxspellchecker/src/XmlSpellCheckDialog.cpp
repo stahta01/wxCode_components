6bebf5e21a9a7a0eed5ccfcfa47a237e069c5adf
jblough <jblough@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2004-03-30 13:05:16 +0000

    Added wxWidgets version check to since syntax highlighting code works differently under 2.4.x and 2.5.x

--- wxspellchecker/src/XmlSpellCheckDialog.cpp
+++ wxspellchecker/src/XmlSpellCheckDialog.cpp
@@ -364,19 +364,20 @@ void XmlSpellCheckDialog::SetMispelledWord(const wxString& strMispelling)
       wxSpellCheckEngineInterface::MispellingContext Context = m_pSpellCheckEngine->GetCurrentMispellingContext();
       pContextText->SetEditable(FALSE);
       pContextText->Clear();
-      /*
-      // This code worked in the 2.4.x branch
-      pContextText->SetValue(Context.GetContext());
-      pContextText->SetSelection(Context.GetOffset(), Context.GetOffset() + Context.GetLength());
-      pContextText->SetStyle(Context.GetOffset(), Context.GetOffset() + Context.GetLength(), wxTextAttr(*wxRED, *wxLIGHT_GREY));
-    */
-      wxString strContext = Context.GetContext();
-      pContextText->SetValue(strContext.Left(Context.GetOffset()));
-      wxColour originalTextColour = pContextText->GetDefaultStyle().GetTextColour();
-      pContextText->SetDefaultStyle(wxTextAttr(*wxRED));
-      pContextText->AppendText(strContext.Mid(Context.GetOffset(), Context.GetLength()));
-      pContextText->SetDefaultStyle(wxTextAttr(originalTextColour));
-      pContextText->AppendText(strContext.Right(strContext.Length() - (Context.GetOffset() + Context.GetLength())));
+      
+      #if wxCHECK_VERSION(2, 5, 0)
+        // This code works in the 2.5.x branch
+        wxString strContext = Context.GetContext();
+        pContextText->SetValue(strContext.Left(Context.GetOffset()));
+        wxColour originalTextColour = pContextText->GetDefaultStyle().GetTextColour();
+        pContextText->SetDefaultStyle(wxTextAttr(*wxRED));
+        pContextText->AppendText(strContext.Mid(Context.GetOffset(), Context.GetLength()));
+        pContextText->SetDefaultStyle(wxTextAttr(originalTextColour));
+        pContextText->AppendText(strContext.Right(strContext.Length() - (Context.GetOffset() + Context.GetLength())));*/
+      #else  
+        // This code works in the 2.4.x branch
+        pContextText->SetValue(Context.GetContext());        pContextText->SetSelection(Context.GetOffset(), Context.GetOffset() + Context.GetLength());        pContextText->SetStyle(Context.GetOffset(), Context.GetOffset() + Context.GetLength(), wxTextAttr(*wxRED, *wxLIGHT_GREY));
+      #endif
     }
   }
   TransferDataToWindow();
