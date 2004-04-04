d11eed3131a1f4d5aaabdaddb97288f8beaca4ca
jblough <jblough@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2004-04-04 02:45:11 +0000

    Now option data is updated from the option dialog.

--- trunk/wxCode/components/wxspellchecker/src/Outlook97LikeDialog.cpp
+++ trunk/wxCode/components/wxspellchecker/src/Outlook97LikeDialog.cpp
@@ -234,6 +234,15 @@ void Outlook97LikeDialog::OnClickOptions(wxCommandEvent& event)
   // Create a really basic dialog that gets dynamically populated
   // with controls based on the m_pSpellCheckEngine->GetOptions();
   SpellCheckerOptionsDialog OptionsDialog(this, "Options", m_pSpellCheckEngine->GetOptions());
-  OptionsDialog.ShowModal();
+  if (OptionsDialog.ShowModal() == wxID_OK)
+  {
+    // Set the modified options
+    OptionsMap* pOptionsMap = OptionsDialog.GetModifiedOptions();
+    if (pOptionsMap)
+    {
+      for (OptionsMap::iterator it = pOptionsMap->begin(); it != pOptionsMap->end(); it++)
+        m_pSpellCheckEngine->SetOption(it->second);
+    }
+  }
 }
 
