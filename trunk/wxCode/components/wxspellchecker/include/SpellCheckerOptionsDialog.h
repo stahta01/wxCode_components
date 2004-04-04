920de809559ec59083619206351580beea79144c
jblough <jblough@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2004-04-04 02:35:48 +0000

    Updated the options dialog to work with directories and update the option data from the dialog.

--- trunk/wxCode/components/wxspellchecker/include/SpellCheckerOptionsDialog.h
+++ trunk/wxCode/components/wxspellchecker/include/SpellCheckerOptionsDialog.h
@@ -9,11 +9,20 @@ public:
   SpellCheckerOptionsDialog(wxWindow* pParent, const wxString& strCaption, OptionsMap* pOptionsMap);
   
   void OnOK(wxCommandEvent& event);
-private:
+  void OnBrowseForDir(wxCommandEvent& event);
+  void OnBrowseForFile(wxCommandEvent& event);
+  OptionsMap* GetModifiedOptions() { return &m_ModifiedOptions; }
+
+  virtual bool TransferDataFromWindow();
+  virtual bool TransferDataToWindow();
+
+  private:
   void CreateControls();
   void PopulateOptionsSizer(wxSizer* pSizer);
   
-  OptionsMap* m_pOptionsMap;
+  OptionsMap m_ModifiedOptions;
+
+  DECLARE_EVENT_TABLE()
 };
 
 #endif // __SPELL_CHECKER_OPTIONS_DIALOG__
