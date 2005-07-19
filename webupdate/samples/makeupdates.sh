d136420c7aa123a8bce37c40bca48a132bf8a568
frm <frm@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2005-07-19 20:09:52 +0000

    added the update packages

---
+++ webupdate/samples/makeupdates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+# Author: Francesco Montorsi
+# RCS-ID: $Id$
+# Purpose: creates the ZIP and TAR.GZ archives used as updates for
+#          the samples contained in this folder; then move the archives
+#          in the ../website folder
+
+
+# create the updates for the given program name ($1 = name, $2 = version)
+function CreateUpdate { 
+    cd $1/v$2
+	zip -9 ../../$1-$2.zip *.exe *.txt
+	tar -czf ../../$1-$2.tar.gz *.exe *.txt
+	cd ../..
+}
+
+#remove old & create new packages
+rm -f *.zip *.tar.gz
+CreateUpdate simple 2.0.3
+CreateUpdate advanced 1.5.0
+
+# for the advanced sample, create also the "addon" package
+cd advanced/v1.5.0 && zip -9 ../../addon-1.5.0.zip addon* && cd ../..
+
+# move the packages
+mv -f *.zip ../website
+mv -f *.tar.gz ../website
