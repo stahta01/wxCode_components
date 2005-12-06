32ea2924b06e792aad41af7d1afecf689a4584c6
wyo <wyo@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2005-12-06 21:30:05 +0000

    Small changes from taken from wxSTC

--- wxscintilla/src/PlatWX.cpp
+++ wxscintilla/src/PlatWX.cpp
@@ -625,7 +625,10 @@ void Window::SetCursor(Cursor curs) {
 #else
        wxCursor wc = wxCursor(cursorId) ;
 #endif
-       GETWIN(id)->SetCursor(wc);
+    if(curs != cursorLast) {
+        GETWIN(id)->SetCursor(wc);
+        cursorLast = curs;
+    }
 }
 
 
