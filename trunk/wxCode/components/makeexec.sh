ad29f0efe55f00e9d079c64c535e94fa3b9a2172
frm <frm@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2006-09-02 09:44:32 +0000

    moved stuff into utils.sh

--- trunk/wxCode/components/makeexec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# the following tells SVN to make executable some files required
-# to build the components on Unix systems
-# Remember to do a svn commit after running this comamnd!
-svn propset svn:executable ON */configure */build/config.* */build/*.sh
