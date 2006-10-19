dcf3ef9932a2c3dc642b3f082742493b56e985c9
frm <frm@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2006-10-19 16:29:30 +0000

    fixed LastRead() problem reported by Stanislav Tishkin

--- trunk/wxCode/components/webupdate/src/md5.cpp
+++ trunk/wxCode/components/webupdate/src/md5.cpp
@@ -9,28 +9,28 @@
 /////////////////////////////////////////////////////////////////////////////
 
 /*
- **********************************************************************
- ** Copyright (C) 1990, RSA Data Security, Inc. All rights reserved. **
- **                                                                  **
- ** License to copy and use this software is granted provided that   **
- ** it is identified as the "RSA Data Security, Inc. MD5 Message     **
- ** Digest Algorithm" in all material mentioning or referencing this **
- ** software or this function.                                       **
- **                                                                  **
- ** License is also granted to make and use derivative works         **
- ** provided that such works are identified as "derived from the RSA **
- ** Data Security, Inc. MD5 Message Digest Algorithm" in all         **
- ** material mentioning or referencing the derived work.             **
- **                                                                  **
- ** RSA Data Security, Inc. makes no representations concerning      **
- ** either the merchantability of this software or the suitability   **
- ** of this software for any particular purpose.  It is provided "as **
- ** is" without express or implied warranty of any kind.             **
- **                                                                  **
- ** These notices must be retained in any copies of any part of this **
- ** documentation and/or software.                                   **
- **********************************************************************
- */
+**********************************************************************
+** Copyright (C) 1990, RSA Data Security, Inc. All rights reserved. **
+**                                                                  **
+** License to copy and use this software is granted provided that   **
+** it is identified as the "RSA Data Security, Inc. MD5 Message     **
+** Digest Algorithm" in all material mentioning or referencing this **
+** software or this function.                                       **
+**                                                                  **
+** License is also granted to make and use derivative works         **
+** provided that such works are identified as "derived from the RSA **
+** Data Security, Inc. MD5 Message Digest Algorithm" in all         **
+** material mentioning or referencing the derived work.             **
+**                                                                  **
+** RSA Data Security, Inc. makes no representations concerning      **
+** either the merchantability of this software or the suitability   **
+** of this software for any particular purpose.  It is provided "as **
+** is" without express or implied warranty of any kind.             **
+**                                                                  **
+** These notices must be retained in any copies of any part of this **
+** documentation and/or software.                                   **
+**********************************************************************
+*/
 
 
 
@@ -56,7 +56,7 @@
 // ----------------
 // MD5 by RSA
 // ----------------
-	
+
 char * MD5End(MD5_CTX *ctx, char *buf)
 {
     int i;
@@ -65,12 +65,12 @@ char * MD5End(MD5_CTX *ctx, char *buf)
 
     if (!buf)
     {
-     	buf = (char *)malloc(33);
+        buf = (char *)malloc(33);
     }
-    
+
     if (!buf)
         return 0;
-    
+
     MD5Final(digest,ctx);
     for (i=0;i<MD5_HASHBYTES;i++) {
         buf[i+i] = hex[digest[i] >> 4];
@@ -81,19 +81,19 @@ char * MD5End(MD5_CTX *ctx, char *buf)
 }
 
 /*
- * Final wrapup - pad to 64-byte boundary with the bit pattern
- * 1 0* (64-bit count of bits processed, MSB-first)
- */
+* Final wrapup - pad to 64-byte boundary with the bit pattern
+* 1 0* (64-bit count of bits processed, MSB-first)
+*/
 void MD5Final(unsigned char digest[16], MD5_CTX *ctx)
 {
     unsigned count;
     unsigned char *p;
 
     /* Compute number of bytes mod 64 */
-    count = (ctx->bits[0] >> 3) & 0x3F; 
+    count = (ctx->bits[0] >> 3) & 0x3F;
 
     /* Set the first char of padding to 0x80.  This is safe since there is
-       always at least one byte free */
+    always at least one byte free */
     p = ctx->in + count;
     *p++ = 0x80;
 
@@ -111,7 +111,7 @@ void MD5Final(unsigned char digest[16], MD5_CTX *ctx)
         memset(ctx->in, 0, 56);
     } else {
         /* Pad block to 56 bytes */
-        memset(p, 0, count - 8);   
+        memset(p, 0, count - 8);
     }
     //[TCC] byteReverse(ctx->in, 14);
 
@@ -120,7 +120,7 @@ void MD5Final(unsigned char digest[16], MD5_CTX *ctx)
     ((unsigned int *) ctx->in)[15] = ctx->bits[1];
 
     MD5Transform(ctx->buf, (unsigned int *) ctx->in);
-    //[TCC] byteReverse((unsigned char *) ctx->buf, 4);   
+    //[TCC] byteReverse((unsigned char *) ctx->buf, 4);
     memcpy(digest, ctx->buf, 16);
     memset((char *) ctx, 0, sizeof(ctx));       /* In case it's sensitive */
 }
@@ -182,7 +182,7 @@ void MD5Update(MD5_CTX *ctx, unsigned char const *buf, unsigned len)
 
 
 /* #define F1(x, y, z) (x & y | ~x & z) */
-#define F1(x, y, z) (z ^ (x & (y ^ z)))   
+#define F1(x, y, z) (z ^ (x & (y ^ z)))
 #define F2(x, y, z) F1(z, x, y)
 #define F3(x, y, z) (x ^ y ^ z)
 #define F4(x, y, z) (y ^ (x | ~z))
@@ -192,10 +192,10 @@ void MD5Update(MD5_CTX *ctx, unsigned char const *buf, unsigned len)
         ( w += f(x, y, z) + data,  w = w<<s | w>>(32-s),  w += x )
 
 /*
- * The core of the MD5 algorithm, this alters an existing MD5 hash to
- * reflect the addition of 16 longwords of new data.  MD5Update blocks
- * the data and converts bytes into longwords for this routine.
- */
+* The core of the MD5 algorithm, this alters an existing MD5 hash to
+* reflect the addition of 16 longwords of new data.  MD5Update blocks
+* the data and converts bytes into longwords for this routine.
+*/
 void MD5Transform(unsigned int buf[4], unsigned int const in[16])
 {
     register unsigned int a, b, c, d;
@@ -205,37 +205,37 @@ void MD5Transform(unsigned int buf[4], unsigned int const in[16])
     c = buf[2];
     d = buf[3];
 
-    MD5STEP(F1, a, b, c, d, in[0] + 0xd76aa478, 7); 
+    MD5STEP(F1, a, b, c, d, in[0] + 0xd76aa478, 7);
     MD5STEP(F1, d, a, b, c, in[1] + 0xe8c7b756, 12);
     MD5STEP(F1, c, d, a, b, in[2] + 0x242070db, 17);
     MD5STEP(F1, b, c, d, a, in[3] + 0xc1bdceee, 22);
-    MD5STEP(F1, a, b, c, d, in[4] + 0xf57c0faf, 7); 
+    MD5STEP(F1, a, b, c, d, in[4] + 0xf57c0faf, 7);
     MD5STEP(F1, d, a, b, c, in[5] + 0x4787c62a, 12);
     MD5STEP(F1, c, d, a, b, in[6] + 0xa8304613, 17);
-    MD5STEP(F1, b, c, d, a, in[7] + 0xfd469501, 22); 
-    MD5STEP(F1, a, b, c, d, in[8] + 0x698098d8, 7);  
-    MD5STEP(F1, d, a, b, c, in[9] + 0x8b44f7af, 12); 
+    MD5STEP(F1, b, c, d, a, in[7] + 0xfd469501, 22);
+    MD5STEP(F1, a, b, c, d, in[8] + 0x698098d8, 7);
+    MD5STEP(F1, d, a, b, c, in[9] + 0x8b44f7af, 12);
     MD5STEP(F1, c, d, a, b, in[10] + 0xffff5bb1, 17);
     MD5STEP(F1, b, c, d, a, in[11] + 0x895cd7be, 22);
-    MD5STEP(F1, a, b, c, d, in[12] + 0x6b901122, 7); 
+    MD5STEP(F1, a, b, c, d, in[12] + 0x6b901122, 7);
     MD5STEP(F1, d, a, b, c, in[13] + 0xfd987193, 12);
     MD5STEP(F1, c, d, a, b, in[14] + 0xa679438e, 17);
     MD5STEP(F1, b, c, d, a, in[15] + 0x49b40821, 22);
 
-    MD5STEP(F2, a, b, c, d, in[1] + 0xf61e2562, 5);  
-    MD5STEP(F2, d, a, b, c, in[6] + 0xc040b340, 9);  
+    MD5STEP(F2, a, b, c, d, in[1] + 0xf61e2562, 5);
+    MD5STEP(F2, d, a, b, c, in[6] + 0xc040b340, 9);
     MD5STEP(F2, c, d, a, b, in[11] + 0x265e5a51, 14);
-    MD5STEP(F2, b, c, d, a, in[0] + 0xe9b6c7aa, 20); 
-    MD5STEP(F2, a, b, c, d, in[5] + 0xd62f105d, 5);  
-    MD5STEP(F2, d, a, b, c, in[10] + 0x02441453, 9); 
+    MD5STEP(F2, b, c, d, a, in[0] + 0xe9b6c7aa, 20);
+    MD5STEP(F2, a, b, c, d, in[5] + 0xd62f105d, 5);
+    MD5STEP(F2, d, a, b, c, in[10] + 0x02441453, 9);
     MD5STEP(F2, c, d, a, b, in[15] + 0xd8a1e681, 14);
-    MD5STEP(F2, b, c, d, a, in[4] + 0xe7d3fbc8, 20); 
-    MD5STEP(F2, a, b, c, d, in[9] + 0x21e1cde6, 5);  
-    MD5STEP(F2, d, a, b, c, in[14] + 0xc33707d6, 9); 
-    MD5STEP(F2, c, d, a, b, in[3] + 0xf4d50d87, 14); 
-    MD5STEP(F2, b, c, d, a, in[8] + 0x455a14ed, 20); 
+    MD5STEP(F2, b, c, d, a, in[4] + 0xe7d3fbc8, 20);
+    MD5STEP(F2, a, b, c, d, in[9] + 0x21e1cde6, 5);
+    MD5STEP(F2, d, a, b, c, in[14] + 0xc33707d6, 9);
+    MD5STEP(F2, c, d, a, b, in[3] + 0xf4d50d87, 14);
+    MD5STEP(F2, b, c, d, a, in[8] + 0x455a14ed, 20);
     MD5STEP(F2, a, b, c, d, in[13] + 0xa9e3e905, 5);
-    MD5STEP(F2, d, a, b, c, in[2] + 0xfcefa3f8, 9);  
+    MD5STEP(F2, d, a, b, c, in[2] + 0xfcefa3f8, 9);
     MD5STEP(F2, c, d, a, b, in[7] + 0x676f02d9, 14);
     MD5STEP(F2, b, c, d, a, in[12] + 0x8d2a4c8a, 20);
 
@@ -243,42 +243,42 @@ void MD5Transform(unsigned int buf[4], unsigned int const in[16])
     MD5STEP(F3, d, a, b, c, in[8] + 0x8771f681, 11);
     MD5STEP(F3, c, d, a, b, in[11] + 0x6d9d6122, 16);
     MD5STEP(F3, b, c, d, a, in[14] + 0xfde5380c, 23);
-    MD5STEP(F3, a, b, c, d, in[1] + 0xa4beea44, 4);  
-    MD5STEP(F3, d, a, b, c, in[4] + 0x4bdecfa9, 11); 
-    MD5STEP(F3, c, d, a, b, in[7] + 0xf6bb4b60, 16); 
+    MD5STEP(F3, a, b, c, d, in[1] + 0xa4beea44, 4);
+    MD5STEP(F3, d, a, b, c, in[4] + 0x4bdecfa9, 11);
+    MD5STEP(F3, c, d, a, b, in[7] + 0xf6bb4b60, 16);
     MD5STEP(F3, b, c, d, a, in[10] + 0xbebfbc70, 23);
-    MD5STEP(F3, a, b, c, d, in[13] + 0x289b7ec6, 4); 
-    MD5STEP(F3, d, a, b, c, in[0] + 0xeaa127fa, 11); 
-    MD5STEP(F3, c, d, a, b, in[3] + 0xd4ef3085, 16); 
-    MD5STEP(F3, b, c, d, a, in[6] + 0x04881d05, 23); 
-    MD5STEP(F3, a, b, c, d, in[9] + 0xd9d4d039, 4);  
+    MD5STEP(F3, a, b, c, d, in[13] + 0x289b7ec6, 4);
+    MD5STEP(F3, d, a, b, c, in[0] + 0xeaa127fa, 11);
+    MD5STEP(F3, c, d, a, b, in[3] + 0xd4ef3085, 16);
+    MD5STEP(F3, b, c, d, a, in[6] + 0x04881d05, 23);
+    MD5STEP(F3, a, b, c, d, in[9] + 0xd9d4d039, 4);
     MD5STEP(F3, d, a, b, c, in[12] + 0xe6db99e5, 11);
     MD5STEP(F3, c, d, a, b, in[15] + 0x1fa27cf8, 16);
-    MD5STEP(F3, b, c, d, a, in[2] + 0xc4ac5665, 23); 
+    MD5STEP(F3, b, c, d, a, in[2] + 0xc4ac5665, 23);
 
     MD5STEP(F4, a, b, c, d, in[0] + 0xf4292244, 6);
     MD5STEP(F4, d, a, b, c, in[7] + 0x432aff97, 10);
     MD5STEP(F4, c, d, a, b, in[14] + 0xab9423a7, 15);
-    MD5STEP(F4, b, c, d, a, in[5] + 0xfc93a039, 21); 
-    MD5STEP(F4, a, b, c, d, in[12] + 0x655b59c3, 6); 
-    MD5STEP(F4, d, a, b, c, in[3] + 0x8f0ccc92, 10); 
+    MD5STEP(F4, b, c, d, a, in[5] + 0xfc93a039, 21);
+    MD5STEP(F4, a, b, c, d, in[12] + 0x655b59c3, 6);
+    MD5STEP(F4, d, a, b, c, in[3] + 0x8f0ccc92, 10);
     MD5STEP(F4, c, d, a, b, in[10] + 0xffeff47d, 15);
-    MD5STEP(F4, b, c, d, a, in[1] + 0x85845dd1, 21); 
-    MD5STEP(F4, a, b, c, d, in[8] + 0x6fa87e4f, 6);  
+    MD5STEP(F4, b, c, d, a, in[1] + 0x85845dd1, 21);
+    MD5STEP(F4, a, b, c, d, in[8] + 0x6fa87e4f, 6);
     MD5STEP(F4, d, a, b, c, in[15] + 0xfe2ce6e0, 10);
-    MD5STEP(F4, c, d, a, b, in[6] + 0xa3014314, 15); 
+    MD5STEP(F4, c, d, a, b, in[6] + 0xa3014314, 15);
     MD5STEP(F4, b, c, d, a, in[13] + 0x4e0811a1, 21);
-    MD5STEP(F4, a, b, c, d, in[4] + 0xf7537e82, 6);  
+    MD5STEP(F4, a, b, c, d, in[4] + 0xf7537e82, 6);
     MD5STEP(F4, d, a, b, c, in[11] + 0xbd3af235, 10);
-    MD5STEP(F4, c, d, a, b, in[2] + 0x2ad7d2bb, 15); 
-    MD5STEP(F4, b, c, d, a, in[9] + 0xeb86d391, 21); 
+    MD5STEP(F4, c, d, a, b, in[2] + 0x2ad7d2bb, 15);
+    MD5STEP(F4, b, c, d, a, in[9] + 0xeb86d391, 21);
 
     buf[0] += a;
     buf[1] += b;
     buf[2] += c;
     buf[3] += d;
 }
- 
+
 
 #ifndef REVERSEBYTE
 #define byteReverse(buf, len)   /* Nothing */
@@ -286,15 +286,15 @@ void MD5Transform(unsigned int buf[4], unsigned int const in[16])
 void byteReverse(unsigned char *buf, unsigned longs);
 
 /*
- * Note: this code is harmless on little-endian machines.
- */
+* Note: this code is harmless on little-endian machines.
+*/
 void byteReverse(unsigned char *buf, unsigned longs)
 {
     unsigned int t;
     do {
         t = (unsigned int) ((unsigned) buf[3] << 8 | buf[2]) << 16 |
             ((unsigned) buf[1] << 8 | buf[0]);
-        *(unsigned int *) buf = t;  
+        *(unsigned int *) buf = t;
         buf += 4;
     } while (--longs);
 }
@@ -308,44 +308,53 @@ void byteReverse(unsigned char *buf, unsigned longs)
 
 wxString wxMD5::GetMD5(const wxString &string)
 {
-	int lenght = string.Len();
-	//wxStringBuffer buf(string, lenght);
+    int lenght = string.Len();
+    //wxStringBuffer buf(string, lenght);
 
-	MD5_CTX ctx;
-	char tmp[40];		// MD5 are fixed sized to 32 chars
-	
-	MD5Init(&ctx);
-	MD5Update(&ctx, (const unsigned char*)string.GetData(), lenght*sizeof(wxChar));
-	MD5End(&ctx, tmp);
+    MD5_CTX ctx;
+    char tmp[40];		// MD5 are fixed sized to 32 chars
 
-	return wxString(tmp, wxConvUTF8);
+    MD5Init(&ctx);
+    MD5Update(&ctx, (const unsigned char*)string.GetData(), lenght*sizeof(wxChar));
+    MD5End(&ctx, tmp);
+
+    return wxString(tmp, wxConvUTF8);
 }
 
 wxString wxMD5::GetFileMD5(wxInputStream &stream)
 {
-    unsigned char buffer[102400];
-	char tmp[40];		// MD5 are fixed sized to 32 chars
+    unsigned char buffer[1024];
+    char tmp[40];         // MD5 are fixed sized to 32 chars
     MD5_CTX ctx;
 
-	if (!stream.IsOk())
-		return wxEmptyString;
+    if (!stream.IsOk())
+        return wxEmptyString;
 
-   	MD5Init(&ctx);
-
-    do
+    MD5Init(&ctx);
+    while(true)
     {
-		if (stream.Read(buffer, sizeof(buffer)).LastRead() <= 0)
-			return wxEmptyString;
+        if ( stream.Read(buffer, sizeof(buffer)).LastRead() <= 0 )
+        {
+            if (stream.Eof())
+            {
+                // no real error - we've just tried to read past EOF
+                MD5End(&ctx, tmp);
+                return wxString(tmp, wxConvUTF8);
+            }
 
-    	MD5Update(&ctx, buffer, stream.LastRead());
-    } while (!stream.Eof());
+            // some I/O error...
+            return wxEmptyString;
+        }
 
-	MD5End(&ctx, tmp);
-    return wxString(tmp, wxConvUTF8);
+        MD5Update(&ctx, buffer, stream.LastRead());
+    }
+
+    // avoid dummy warnings
+    return wxEmptyString;
 }
 
 wxString wxMD5::GetFileMD5(const wxString &filename)
-{ 
-	wxFileInputStream stream(filename); 
-	return GetFileMD5(stream); 
+{
+    wxFileInputStream stream(filename);
+    return GetFileMD5(stream);
 }
