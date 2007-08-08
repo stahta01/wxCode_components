da8c7ed92468ffa009362ae428f8151134a070a0
frm <frm@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2007-08-08 08:08:06 +0000

    the name of the component must match the name under which it's registered in wxCode DB

--- rlayout/website/index.php
+++ rlayout/website/index.php
@@ -10,7 +10,7 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-AU">
 	<?php
-      $compname = "RecursiveLayout";
+      $compname = "wxRL";
       $subdir = "rlayout";
       require("compwebsite.inc.php");
   ?>
