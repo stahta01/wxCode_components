b3ceca836146a80f6830a28c9e690650ece0c842
janknepper <janknepper@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2007-11-13 16:08:43 +0000

    Added ODBC (initially based on wxODBC) to CVS. This however does not work as of yet.

---
+++ trunk/wxCode/components/tablectrl/samples/ODBC/ChildFrame.hpp
@@ -0,0 +1,55 @@
+// Soli Deo Gloria!
+
+
+
+#ifndef CHILDFRAME_HPP
+#define CHILDFRAME_HPP
+
+
+
+#include <wx/TableCtrl.h>
+
+#include "ODBCTable.h"
+#include "MainFrame.h"
+#include "ChildFrame.h"
+
+
+
+class  ChildFrame : public  wxMDIChildFrame
+{
+   private   :
+      typedef wxMDIChildFrame
+                           super;
+                           
+   public    :
+      static const long    DEFAULTSTYLE;
+                                 
+   private   :
+      
+#if 0
+      CTSession *          session;
+      CTTable *            table;
+      CTRecord *           record;
+      
+      NADCTreeTable *      t;
+#endif      
+      wxDb *               db;
+      ODBCTable *          table;
+      
+      wxSizer *            sizer;
+      
+      wxTableCtrl *        tc;
+      
+   public    :
+      ChildFrame  ( MainFrame *, wxWindowID = wxID_ANY, const wxString & = wxEmptyString, const wxPoint & = wxDefaultPosition, const wxSize & = wxDefaultSize, long = DEFAULTSTYLE );
+      ~ChildFrame ();
+
+#if 0      
+      bool                 Open  ( CTSession *, const wxString & );
+#endif
+      bool                 Open  ( wxDb *, ODBCTable * );    
+};
+
+
+
+#endif
