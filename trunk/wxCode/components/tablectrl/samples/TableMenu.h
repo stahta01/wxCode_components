843254dd1ede6cedb089b982940acaf709ef80d0
janknepper <janknepper@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2008-02-06 23:48:18 +0000

    Moved "&Table" menu id's to TableMenu.h.

---
+++ trunk/wxCode/components/tablectrl/samples/TableMenu.h
@@ -0,0 +1,27 @@
+#ifndef TABLEMENU_H
+#define TABLEMENU_H
+
+
+
+const wxWindowID        IDM_BEGIN            = 1000;
+
+const wxWindowID        IDM_MULTISELECT      = 1000;
+const wxWindowID        IDM_DRAGANDDROP      = 1001;
+const wxWindowID        IDM_GRIDHORIZONTAL   = 1002;
+const wxWindowID        IDM_GRIDVERTICAL     = 1003;
+const wxWindowID        IDM_CHECKBOXES       = 1004;
+const wxWindowID        IDM_FOCUSRECT        = 1005;
+const wxWindowID        IDM_TOOLTIPS         = 1006;
+
+const wxWindowID        IDM_NATIVEHEADER     = 1007;
+
+const wxWindowID        IDM_SWITCH           = 1011;
+const wxWindowID        IDM_SWITCH_FAST      = IDM_SWITCH + wxTableCtrl :: Body :: Mode_FAST;
+const wxWindowID        IDM_SWITCH_MERGE     = IDM_SWITCH + wxTableCtrl :: Body :: Mode_MERGE;
+const wxWindowID        IDM_SWITCH_ROLL      = IDM_SWITCH + wxTableCtrl :: Body :: Mode_ROLL;
+
+const wxWindowID        IDM_END              = 2000;
+
+
+
+#endif
