de2d969d7c48ad9ad85771cf371fe2b8802cab06
janknepper <janknepper@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2008-02-25 20:53:36 +0000

    Added log window for event's. Added IDM_COLUMNRESIZING.

--- trunk/wxCode/components/tablectrl/samples/cTreePlus/ChildFrame.cpp
+++ trunk/wxCode/components/tablectrl/samples/cTreePlus/ChildFrame.cpp
@@ -14,6 +14,7 @@
 #include <wx/wxprec.h>
 
 #include <wx/filename.h>
+#include <wx/textctrl.h>
 
 #include <wx/TableCtrl.hpp>
 #include <wx/cTreePlus/cTreeTable.hpp>
@@ -31,6 +32,18 @@ const long              ChildFrame :: DEFAULTSTYLE = wxMINIMIZE_BOX | wxMAXIMIZE
 BEGIN_EVENT_TABLE ( ChildFrame, wxMDIChildFrame )
    EVT_MENU_RANGE ( IDM_BEGIN , 
                     IDM_END   , ChildFrame :: OnMenu  )
+                    
+   EVT_TABLE_COLUMN_BEGIN_MOVE   ( wxID_ANY  , ChildFrame :: OnTableColumn )
+   EVT_TABLE_COLUMN_MOVING       ( wxID_ANY  , ChildFrame :: OnTableColumn )
+   EVT_TABLE_COLUMN_MOVED        ( wxID_ANY  , ChildFrame :: OnTableColumn )
+
+   EVT_TABLE_COLUMN_BEGIN_SIZE   ( wxID_ANY  , ChildFrame :: OnTableColumn )
+   EVT_TABLE_COLUMN_SIZING       ( wxID_ANY  , ChildFrame :: OnTableColumn )
+   EVT_TABLE_COLUMN_SIZED        ( wxID_ANY  , ChildFrame :: OnTableColumn )
+   
+   EVT_TABLE_COLUMN_LEFT_CLICK   ( wxID_ANY  , ChildFrame :: OnTableColumn )
+   EVT_TABLE_COLUMN_RIGHT_CLICK  ( wxID_ANY  , ChildFrame :: OnTableColumn )
+   EVT_TABLE_COLUMN_MIDDLE_CLICK ( wxID_ANY  , ChildFrame :: OnTableColumn )
 END_EVENT_TABLE   ()
 
 
@@ -87,6 +100,25 @@ void  ChildFrame :: OnMenu ( wxCommandEvent &  ce )
          
          break;
 
+      case IDM_COLUMNRESIZING    :
+         columnresizing = ! columnresizing;
+
+         if ( tc != 0 )
+         {
+            wxTableCtrl :: Header &    header   = tc -> GetHeader ();
+            size_t                     COLS     = header.size ();
+            
+            for ( size_t  i = 0 ; i < COLS ; ++i )
+            {
+               wxTableCtrl :: Column *    column   = header.Find ( i );
+               
+               if ( column != 0 )
+                  column -> Resize ( columnresizing );
+            }
+         }
+
+         break;
+         
       case IDM_SWITCH_FAST       :
       case IDM_SWITCH_MERGE      :
       case IDM_SWITCH_ROLL       :
@@ -98,21 +130,35 @@ void  ChildFrame :: OnMenu ( wxCommandEvent &  ce )
 
 
 
+void  ChildFrame :: OnTableColumn ( wxTableEvent &  te )
+{
+   const wxRect &    rect     = te.GetRect ();
+   const wxTableCtrl :: Column *
+                     column   = te.GetColumn ();
+
+   wxLogMessage ( "wxTableEvent %d (%d,%d-%d,%d) %s", te.GetEventType (), rect.GetX (), rect.GetY (), rect.GetWidth  (), rect.GetHeight (), ( column != 0 ) ? column -> Name () : "" );
+   
+   te.Skip  ();
+}
+
+
+
 ChildFrame :: ChildFrame ( MainFrame *  _window, wxWindowID  _id, const wxString &  _title, const wxPoint &  _point, const wxSize &  _size, long  _style )
    : wxMDIChildFrame ( _window, _id, _title, _point, _size, _style )
 {
-   session  = 0;
-   table    = 0;
-   t        = 0;
+   session        = 0;
+   table          = 0;
+   t              = 0;
+   columnresizing = true;
    
    wxMenuBar *    menubar     = new  wxMenuBar  ();
    wxMenu *       filemenu    = new  wxMenu     ();
    wxMenu *       tablemenu   = new  wxMenu     ();
    wxMenu *       switchmenu  = new  wxMenu     ();
    
-   filemenu -> Append            ( wxID_OPEN    , "&Open..."   , wxEmptyString );
-   filemenu -> AppendSeparator   ();
-   filemenu -> Append            ( wxID_EXIT    , "E&xit"      , wxEmptyString );
+   filemenu    -> Append            ( wxID_OPEN    , "&Open..."   , wxEmptyString );
+   filemenu    -> AppendSeparator   ();
+   filemenu    -> Append            ( wxID_EXIT    , "E&xit"      , wxEmptyString );
    
    switchmenu  -> Append            ( IDM_SWITCH_FAST    , "&Fast"            , "Switch columns in \"FAST\" mode"                         , wxITEM_RADIO );
    switchmenu  -> Append            ( IDM_SWITCH_MERGE   , "&Merge"           , "Switch columns in \"MERGE\" mode"                        , wxITEM_RADIO );
@@ -128,18 +174,26 @@ ChildFrame :: ChildFrame ( MainFrame *  _window, wxWindowID  _id, const wxString
    tablemenu   -> Append            ( IDM_TOOLTIPS       , "&ToolTips"        , "Show Tooltips"                                           , wxITEM_CHECK );
    tablemenu   -> AppendSeparator   ();
    tablemenu   -> Append            ( IDM_NATIVEHEADER   , "&Native Header"   , "Native Header"                                           , wxITEM_CHECK );
+   tablemenu   -> Append            ( IDM_COLUMNRESIZING , "Column &Resizing" , "Allow Column Resizing"                                   , wxITEM_CHECK );
    
    menubar     -> Append            ( filemenu           , "&File"            );
    menubar     -> Append            ( tablemenu          , "&Table"           );
+   
+   menubar     -> Check             ( IDM_COLUMNRESIZING , true );
    
    SetMenuBar  ( menubar );
 
    sizer    = new  wxBoxSizer ( wxVERTICAL );
    
-   sizer    -> Add   ( tc  = new  wxTableCtrl   ( this ), 1, wxALL | wxEXPAND, 0 );
+   sizer    -> Add   ( tc  = new  wxTableCtrl   ( this )                                                                            , 1, wxALL | wxEXPAND, 0 );
+   sizer    -> Add   ( log = new  wxTextCtrl    ( this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxTE_MULTILINE ) , 0, wxALL | wxEXPAND, 0 );
    
-   tc -> SetToolTip  ( true );
-   tc -> DragAndDrop ( true );
+   tc    -> SetToolTip  ( true );
+   tc    -> DragAndDrop ( true );
+   
+   log   -> SetMinSize  ( wxSize ( wxDefaultCoord, 64 ) );
+   
+   old   = wxLog :: SetActiveTarget ( new  wxLogTextCtrl ( log ) );
    
    SetSizer ( sizer );
 }
@@ -148,6 +202,8 @@ ChildFrame :: ChildFrame ( MainFrame *  _window, wxWindowID  _id, const wxString
 
 ChildFrame :: ~ChildFrame ()
 {
+   delete  wxLog :: SetActiveTarget ( old );
+   
    tc       -> Table          ( 0 );
    
    delete  t;
