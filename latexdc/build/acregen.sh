6692bd1c174a50fcd7584590b5c3c85189c44da4
frm <frm@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2008-03-10 12:24:13 +0000

    fixed version check

--- latexdc/build/acregen.sh
+++ latexdc/build/acregen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Author: Francesco Montorsi
-# RCS-ID: $Id: acregen.sh 505 2007-03-31 10:31:46Z frm $
+# RCS-ID: $Id: acregen.sh,v 1.4 2006/11/07 11:44:59 frm Exp $
 # Creation date: 14/9/2005
 #
 # A simple script to generate the configure script for a wxCode component
@@ -14,7 +14,7 @@
 # called when an old version of aclocal is found
 function aclocalold()
 {
-    echo "Your aclocal version is  $aclocal_maj.$aclocal_min.$aclocal_rel"
+    echo "Your aclocal version is $aclocal_maj.$aclocal_min.$aclocal_rel"
     echo "Your automake installation is too old; please install automake >= $aclocal_minimal_maj.$aclocal_minimal_min.$aclocal_minimal_rel"
     echo "You can download automake from ftp://sources.redhat.com/pub/automake/"
     exit 1
@@ -30,23 +30,18 @@ aclocal_minimal_maj=1
 aclocal_minimal_min=9
 aclocal_minimal_rel=6
 
-majok=$(expr $aclocal_maj \>= $aclocal_minimal_maj)
-minok=$(expr $aclocal_min \>= $aclocal_minimal_min)
-relok=$(expr $aclocal_rel \>= $aclocal_minimal_rel)
+majok=$(($aclocal_maj > $aclocal_minimal_maj))
+minok=$(($aclocal_maj == $aclocal_minimal_maj && $aclocal_min > $aclocal_minimal_min))
+relok=$(($aclocal_maj == $aclocal_minimal_maj && $aclocal_min == $aclocal_minimal_min && \
+         $aclocal_rel >= $aclocal_minimal_rel))
 
-if [[ "$majok" = "0" ]]; then aclocalold; fi
-if [[ "$majok" = "1" && "$minok" = "0" ]]; then aclocalold; fi
-if [[ "$majok" = "1" && "$minok" = "1" && "$relok" = 0 ]]; then aclocalold; fi
+versionok=$(($majok == 1 || $minok == 1 || $relok == 1))
+if [[ "$versionok" = "0" ]]; then aclocalold; fi
 
 # we can safely proceed
 me=$(basename $0)
 path=${0%%/$me}        # path from which the script has been launched
 current=$(pwd)
 cd $path
-
-# NOTE: we add /usr/local/share/aclocal include path to the "aclocal" call
-#       because usually bakefile gets installed into /usr/local prefix but
-#       by default aclocal only looks into /usr/share/aclocal
-#       (you can test this doing `aclocal --print-ac-dir`)
-aclocal -I /usr/local/share/aclocal && autoconf && mv configure ..
+aclocal && autoconf && mv configure ..
 cd $current
