76557c04a9f56d9292ce108c5b218e38a9cbfc4d
troelsk <troelsk@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2009-11-11 09:13:48 +0000

    Missing from r1066

---
+++ trunk/wxCode/components/stedit/src/wxext.h
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+// File:        wxext.h
+// Purpose:     wxWidgets extensions
+// Author:      Troels K
+// Created:     2009-11-11
+// RCS-ID:      
+// Copyright:   (c) John Labenski, Troels K
+// Licence:     wxWidgets licence
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WXEXT_H__
+#define __WXEXT_H__
+
+#define WXK_HELP       WXK_F1
+#define WXK_FULLSCREEN WXK_F11
+
+WX_DECLARE_OBJARRAY(wxAcceleratorEntry, AcceleratorArray);
+
+WXDLLIMPEXP_STEDIT bool wxLocale_Init(wxLocale*, const wxString& exetitle, enum wxLanguage lang = wxLANGUAGE_DEFAULT);
+
+#endif // __WXEXT_H__
