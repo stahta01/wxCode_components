5d7ecf8f9259142137f33edf35ebe5f2e9a4583f
irccircle <irccircle@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2010-03-24 11:10:28 +0000

    Added info to website component page

--- libircclient/website/index.php
+++ libircclient/website/index.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?php
   ////////////////////////////////////////////////////////////////////////////////////////////
-  // Author: YOUR_NAME
-  // Creation date: someday/somemonth/someyear
+  // Author: Andrea Zanellato
+  // Creation date: 24/03/2010
   // RCS-ID: $Id: index.php 505 2007-03-31 10:31:46Z frm $
   // Notes: the index page of the website of this component
   ////////////////////////////////////////////////////////////////////////////////////////////
@@ -10,8 +10,8 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-AU">
 	<?php
-      $compname = "MYCOMPNAME";
-      $subdir = "MYCOMPSUBDIR";
+      $compname = "LibIRCClient";
+      $subdir = "libircclient";
       require("compwebsite.inc.php");
   ?>
 
