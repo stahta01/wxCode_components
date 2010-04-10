be469f1686b1aca0f91f05461a53aecb8c2d7696
troelsk <troelsk@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2010-04-10 15:20:51 +0000

    wxAboutBox

--- stedit/samples/stedit/link.cpp
+++ stedit/samples/stedit/link.cpp
@@ -13,11 +13,14 @@
          #pragma comment(lib, "wxmsw29ud_html.lib")
          #pragma comment(lib, "wxmsw29ud_core.lib")
          #pragma comment(lib, "wxmsw29ud_stc.lib")
+         #pragma comment(lib, "wxmsw29ud_adv.lib")
+         #pragma comment(lib, "wxmsw29ud_adv.lib")
       #else
          #pragma comment(lib, "wxbase29u.lib")
          #pragma comment(lib, "wxmsw29u_html.lib")
          #pragma comment(lib, "wxmsw29u_core.lib")
          #pragma comment(lib, "wxmsw29u_stc.lib")
+         #pragma comment(lib, "wxmsw29u_adv.lib")
       #endif
    #else
       #ifdef __WXDEBUG__
@@ -25,11 +28,13 @@
          #pragma comment(lib, "wxmsw29d_html.lib")
          #pragma comment(lib, "wxmsw29d_core.lib")
          #pragma comment(lib, "wxmsw29d_stc.lib")
+         #pragma comment(lib, "wxmsw29d_adv.lib")
       #else
          #pragma comment(lib, "wxbase29.lib")
          #pragma comment(lib, "wxmsw29_html.lib")
          #pragma comment(lib, "wxmsw29_core.lib")
          #pragma comment(lib, "wxmsw29_stc.lib")
+         #pragma comment(lib, "wxmsw29_adv.lib")
       #endif
    #endif
 #elif (wxVERSION_NUMBER >= 2800)
@@ -39,11 +44,13 @@
          #pragma comment(lib, "wxmsw28ud_html.lib")
          #pragma comment(lib, "wxmsw28ud_core.lib")
          #pragma comment(lib, "wxmsw28ud_stc.lib")
+         #pragma comment(lib, "wxmsw28ud_adv.lib")
       #else
          #pragma comment(lib, "wxbase28u.lib")
          #pragma comment(lib, "wxmsw28u_html.lib")
          #pragma comment(lib, "wxmsw28u_core.lib")
          #pragma comment(lib, "wxmsw28u_stc.lib")
+         #pragma comment(lib, "wxmsw28u_adv.lib")
       #endif
    #else
       #ifdef __WXDEBUG__
@@ -51,11 +58,13 @@
          #pragma comment(lib, "wxmsw28d_html.lib")
          #pragma comment(lib, "wxmsw28d_core.lib")
          #pragma comment(lib, "wxmsw28d_stc.lib")
+         #pragma comment(lib, "wxmsw28d_adv.lib")
       #else
          #pragma comment(lib, "wxbase28.lib")
          #pragma comment(lib, "wxmsw28_html.lib")
          #pragma comment(lib, "wxmsw28_core.lib")
          #pragma comment(lib, "wxmsw28_stc.lib")
+         #pragma comment(lib, "wxmsw28_adv.lib")
       #endif
    #endif
 #endif
