819f874101e734b2ac47f21cdcc194bd963cf545
troelsk <troelsk@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2010-08-05 17:01:49 +0000

    Make it easy to add accelerators

--- trunk/wxCode/components/stedit/include/wx/stedit/stedefs.h
+++ trunk/wxCode/components/stedit/include/wx/stedit/stedefs.h
@@ -37,7 +37,7 @@
 #define STE_APPNAME           wxT("wxstedit")
 #define STE_APPDISPLAYNAME    wxT("wxStEdit")
 #define STE_VERSION_STRING    STE_APPDISPLAYNAME wxT(" 1.2.6")
-#define STE_VERSION_STRING_SVN STE_VERSION_STRING wxT(" svn r1402")
+#define STE_VERSION_STRING_SVN STE_VERSION_STRING wxT(" svn r1406")
 
 // For non-Unix systems (i.e. when building without a configure script),
 // users of this component can use the following macro to check if the
