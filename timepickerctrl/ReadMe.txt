6d1d8b9f656d10bea43a511d7177ac4538e9f3e8
greenbreen <greenbreen@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2011-02-15 18:50:53 +0000

    Copied template files to timepickerctrl

---
+++ timepickerctrl/ReadMe.txt
@@ -0,0 +1,13 @@
+ 
+ wxCode component template ReadMe
+ --------------------------------
+ 
+ RCS-ID: $Id$
+
+
+ The content of this readme have been moved in this page:
+
+       http://wxcode.sourceforge.net/template.php
+
+
+ 
---
+++ timepickerctrl/include/wx/mycomp.h
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        mycomp.h
+// Purpose:     short description here
+// Author:      Name Surname
+// Created:     someyear/somemonth/someday
+// RCS-ID:      $Id$
+// Copyright:   (c) YEAR Name Surname
+// Licence:     wxWidgets licence
+/////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _WX_MYCOMP_H_
+#define _WX_MYCOMP_H_
+
+// wxWidgets headers
+#include "wx/...."
+
+
+
+#endif // _WX_MYCOMP_H_
