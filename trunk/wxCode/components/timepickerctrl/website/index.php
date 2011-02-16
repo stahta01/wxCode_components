39d956e7256cd2d6dbd49da198a0ab3a429febfe
greenbreen <greenbreen@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2011-02-16 18:02:11 +0000

    Updated website and documentation.

--- trunk/wxCode/components/timepickerctrl/website/index.php
+++ trunk/wxCode/components/timepickerctrl/website/index.php
@@ -1,84 +1,62 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<?php
-  ////////////////////////////////////////////////////////////////////////////////////////////
-  // Author: Paul Breen (greenbreen@yahoo.com); adapted from the generic wxSpinCtrl by Vadim Zeitlin
-  // Creation date: January 3, 2011
-  // RCS-ID: $Id$
-  // Notes: the index page of the website of this component
-  ////////////////////////////////////////////////////////////////////////////////////////////
-?>
-
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en-AU">
-	<?php
-      $compname = "TimePickerCtrl";
-      $subdir = "timepickerctrl";
-      require("compwebsite.inc.php");
-  ?>
-
-	<!-- NOTE on the write_h1() php function:
-         - first arg: the label of the h1 tag
-         - second arg: the ID of the h1 tag (defaults to "")
-         - third arg: if TRUE the TOP link is added; if FALSE it is omitted (defaults to TRUE)
-         use FALSE as third arg for the first h1 tag of the page since it must
-         not have the TOP link...
-  -->
-
-
-	<!-- NOTE on SCREENSHOTS:
-       to write screenshots link with automatic thumbnail-generation, you can write something like:
-
-       <?php writeScreenshotLink("myshot.png", "MYCOMP running with wxMSW 2.5.3"); ?>
-
-	     where the 'myshot.png' file is a file that you have uploaded in
-          /home/groups/w/wx/wxcode/htdocs/screenshots/MYCOMPSUBDIR
-       you can do the upload with scp from unix or (I suggest), WinSCP (http://winscp.net)
-       from win32... more info in the wxCode\template\readme.txt file.
-
-       To put something centered in this page, put it inside a
-
-          <div class="center">...</div>
-
-       block...
-  -->
-	<br/><?php write_h1("Screenshots"); ?>
-	<p>Description</p>
+   <body>
+      <br/>
+      <H1 align="center">TimePickerCtrl version 0.1</H1>
+      <H2>Screenshots</H2>
+         <table width="100%">
+            <tr><td width="50%">
+                  <img src="image_win.png" alt="image_win.png"/>
+               	<p>TimePickerCtrl on Windows</p>
+            </td><td width="50%">
+                  <img src="image_linux.png" alt="image_linux.png"/>
+                  <p>TimePickerCtrl on Linux</p>
+            </td></tr>
+         </table>
   <div class="center">
-    <?php writeScreenshotLink("myshot.png", "description goes here"); ?>
   </div>
-
-
-
-  <!-- NOTE ON THE SECTIONS TO INSERT HERE: you are free to add anything you think
-       that will make your component more appealing: some sample snippet of code,
-       docs, bugs & feature requests... -->
-  <br/><?php write_h1("Usage sample"); ?>
-  <?php writeSnippet('
-// sample CPP snippet which shows how to use this component:
-myComp *newcomp = new myComp();
-newcomp->SetAAAA("aaa");
-newcomp->SetBBBB("bbb");
-
-// inside this section we can use any character we want except
-// for the single quote char: if you need to use it you should
-// escape it in this way: \'
-newcomp->Show();
-'); ?>
-
-
-  <!-- NOTE TO THE DOCUMENTATION: for uploading you component docs in the website,
-       see the ReadMe.txt file at wxCode\template... -->
-  <br/><?php write_h1("Documentation"); ?>
+   <br/>
+   <H2>Usage sample</H2>
+      <p><pre>
+         #include "timepickerctrl.h"
+         ...
+         class MyWindow : public wxFrame{
+         public:
+            MyWindow(...);
+            ...
+            void OnTimePicker(wxCommandEvent& event); // Handler for TimePickerCtrl changed events
+            ...
+            TimePickerCtrl m_timepicker;
+            ...
+         };
+         ...
+         BEGIN_EVENT_TABLE(MyWindow,wxFrame)
+            ...
+            EVT_TIMEPICKER(TimePickerCtrlFrame::OnTimePicker)
+            ...
+         END_EVENT_TABLE()
+         ...
+         MyWindow::MyWindow(...){
+            ...
+            m_timepicker = new TimePickerCtrl(this,ID_CUSTOM1,wxEmptyString,wxDefaultPosition,wxSize(118,45),0);
+            ...
+         }
+         ...
+         void Mywindow::OnTimePicker(wxCommandEvent& event){
+            // Add code here to handle events triggered by the TimePickerCtrl
+            ...
+         }
+         ...</pre>
+  <br/><H2>Documentation</H2>
   <p>The documentation for this component is available online
-  <a href="../../docs/MYCOMPSUBDIR/">here</a>.</p>
+  <a href="../docs/html/">here</a>.</p>
 
 
-	<br/><?php write_h1("Known bugs"); ?>
+	<br/><H2>Known bugs</H2>
   <p>None. To submit a bug report please look at <a href="../../support.php">wxCode support page</a>.</p>
 
     
-	<br/><?php write_h1("Future enhancements"); ?>
-  <p>None. To submit a feature request please look at <a href="../../support.php">wxCode support page</a>.</p>
-    
-
-	<br/><?php require("footer.inc.php"); ?>
+	<br/><H2>Future enhancements</H2>
+  <p>Currently the time format is fixed. Depending on public interest and my available time, I may implement customization.  To submit a feature request please look at <a href="../../support.php">wxCode support page</a>.</p>
+</body>
 </html>
