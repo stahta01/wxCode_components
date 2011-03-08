7079f3345615080f23ea0f5c29598b9b3e3b4ec0
jrl1 <jrl1@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2011-03-08 20:19:07 +0000

    Update autoconf from last rebake

--- trunk/wxCode/components/stedit/build/acregen.sh
+++ trunk/wxCode/components/stedit/build/acregen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Author: Francesco Montorsi
-# RCS-ID: $Id: acregen.sh,v 1.7 2008/03/10 12:23:26 frm Exp $
+# RCS-ID: $Id: acregen.sh 725 2008-03-10 12:22:31Z frm $
 # Creation date: 14/9/2005
 #
 # A simple script to generate the configure script for a wxCode component
@@ -10,6 +10,12 @@
 # - able to be called from any folder
 #   (i.e. you can call it typing 'build/acregen.sh', not only './acregen.sh')
 
+# You may pass optional args to aclocal on the command line.
+# If you are using a local copy (newer perhaps) of bakefile and have a system
+# wide bakefile installed you need to force aclocal to use the bakefile*.m4
+# files from the local copy and not from /usr/share/aclocal/bakefile*.m4
+# E.G. $./acregen.sh "--acdir=/path/to/bakefile-x.y.z/share/aclocal"
+ACLOCAL_ARGS=$1
 
 # called when an old version of aclocal is found
 function aclocalold()
@@ -26,9 +32,6 @@ aclocal_maj=`echo $aclocal_verfull | sed 's/aclocal (GNU automake) \([0-9]*\).\(
 aclocal_min=`echo $aclocal_verfull | sed 's/aclocal (GNU automake) \([0-9]*\).\([0-9]*\).\([0-9]*\).*/\2/'`
 aclocal_rel=`echo $aclocal_verfull | sed 's/aclocal (GNU automake) \([0-9]*\).\([0-9]*\).\([0-9]*\).*/\3/'`
 
-# For versions without a release number, set it to 0
-if [ "a$aclocal_rel" = "a" ]; then aclocal_rel=0; fi
-
 aclocal_minimal_maj=1
 aclocal_minimal_min=9
 aclocal_minimal_rel=6
@@ -38,15 +41,13 @@ minok=$(($aclocal_maj == $aclocal_minimal_maj && $aclocal_min > $aclocal_minimal
 relok=$(($aclocal_maj == $aclocal_minimal_maj && $aclocal_min == $aclocal_minimal_min && \
          $aclocal_rel >= $aclocal_minimal_rel))
 
-
 versionok=$(($majok == 1 || $minok == 1 || $relok == 1))
 if [[ "$versionok" = "0" ]]; then aclocalold; fi
-
 
 # we can safely proceed
 me=$(basename $0)
 path=${0%%/$me}        # path from which the script has been launched
 current=$(pwd)
 cd $path
-aclocal && autoconf && mv configure ..
+aclocal $ACLOCAL_ARGS && autoconf && mv configure ..
 cd $current
