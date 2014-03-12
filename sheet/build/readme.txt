b90f17a559780c11c77f3d2889255a54383c4eb3
jrl1 <jrl1@117ff65d-bb0e-4caa-9e7a-4afe51e3d5b6>
2014-03-12 01:40:51 +0000

    plotctrl, sheet, wxthings - remove old build files.

--- sheet/build/readme.txt
+++ sheet/build/readme.txt
@@ -1,11 +1,1 @@
-The minimal files for wxcode bakefile are from wxCode/build/
-
-bakefiles/empty.bkl.template -> component.bkl and edit
-bakefiles/Bakefiles.bkgen.template -> Bakefiles.bkgen edit to point to component.bkl
-autoconf/configure.ac
-autoconf/config.sub
-autoconf/config.guess
-autoconf/install.sh
-autoconf/acregen.sh
-
-Create make/project files in MSW using bakefile_gen in build dir
+Build using cmake.
--- sheet/src/sheetspt.cpp
+++ sheet/src/sheetspt.cpp
@@ -45,14 +45,19 @@ BEGIN_EVENT_TABLE( wxSheetSplitter, wxWindow )
     EVT_SHEET_SPLIT_BEGIN  ( wxID_ANY, wxSheetSplitter::OnSplit )
     EVT_SHEET_VIEW_CHANGED ( wxID_ANY, wxSheetSplitter::OnViewChanged )
 
+#if !wxCHECK_VERSION(2,9,3)
     WX_EVENT_TABLE_CONTROL_CONTAINER(wxSheetSplitter)
+#endif // !wxCHECK_VERSION(2,9,3)
 END_EVENT_TABLE()
 
+#if !wxCHECK_VERSION(2,9,3)
 #if wxCHECK_VERSION(2, 7, 0)
     WX_DELEGATE_TO_CONTROL_CONTAINER(wxSheetSplitter, wxWindow);
 #else
     WX_DELEGATE_TO_CONTROL_CONTAINER(wxSheetSplitter);
 #endif // wxCHECK_VERSION(2, 7, 0)
+#endif // !wxCHECK_VERSION(2,9,3)
+
 
 void wxSheetSplitter::Init()
 {
